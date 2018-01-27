f9f240e80d051f65d454d2e243c1bae31b856421
toyan <toyan@toyan-VirtualBox>
2018-01-26 23:32:55 -0500

    Intital Hibernate Check in!

---
+++ hibernate-examples/src/main/java/org/south/hibernate_examples/drivers/EmbeddedTables.java
@@ -0,0 +1,13 @@
+package org.south.hibernate_examples.drivers;
+
+/**
+ * Hello world!
+ *
+ */
+public class EmbeddedTables 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
